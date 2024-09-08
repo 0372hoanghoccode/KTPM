0461bbb1a37fab0efffb554a615be7994ab76308
0372hoanghoccode <144258754+HoangHoan04@users.noreply.github.com>
2024-09-09 02:17:56 +0700

    update-home-page

--- src/GUI/Panel/TrangChu.java
+++ src/GUI/Panel/TrangChu.java
@@ -14,9 +14,9 @@ public class TrangChu extends JPanel {
     JPanel info[];
     JLabel title, subTit, infoDetail[], objDetail[], objDetail1[], infoIcon[];
         String[][] getSt = {
-        {"SỨ MỆNH", "convenient_100px.svg", "<html>Hiện nay, Công ty Chúng tôi đã và đang <br><br>ngày càng nỗ lực hơn trong hoạt động <br><br>sản xuất kinh doanh, tiếp tục góp phần <br><br>vào sự nghiệp phát triển “văn hóa <br><br>đọc”, làm cho những giá trị vĩnh hằng <br><br> của sách ngày càng thấm sâu vào đời<br><br> sống văn hóa tinh thần của xã hội, nhằm<br><br> góp phần tích cực, đáp ứng yêu cầu<br><br> nâng cao dân trí, bồi dưỡng nhân tài <br><br> và nguồn nhân lực cho sự nghiệp công <br><br> nghiệp hóa, hiện đại hóa đất nước .</html>"},
-        {"BẢO MẬT & HIỆU QUẢ", "secure_100px.svg", "<html>Thông tin cá nhân và thông tin liên quan đến <br><br> sách mượn thường được bảo mật và chỉ được<br><br> truy cập bởi người dùng hoặc những người được <br><br> ủy quyền Nhờ vào tính năng đặc biệt của mã <br><br> ISBN giúp xác định được thông tin về từng cuốn sách <br><br> một cách nhanh chóng và chính xác, giúp cho việc <br><br> quản lý sách được thực hiện một cách hiệu quả hơn..</html>"},
-        {"HƯỚNG PHÁT TRIỂN ", "effective_100px.svg", "<html>Cửa hàng có thể cung cấp các sản phẩm sách tái chế hoặc <br><br> triển khai các hoạt động bảo vệ môi trường, như giảm thiểu<br><br> việc sử dụng túi nhựa, khuyến khích khách hàng tái sử dụng <br><br>túi vải, và tham gia vào các hoạt động cộng đồng về bảo vệ <br><br> môi trường. Mở rộng dịch vụ thu mua, trao đổi và bán lại<br><br>sách cũ, giúp khách hàng có thêm lựa chọn và thúc đẩy sự <br><br> bền vững trong việc sử dụng sách.</html>"},
+        {"SỨ MỆNH", "convenient_100px.svg",         "<html>Hiện nay, Công ty chúng tôi đã và đang ngày càng nỗ lực<br><br> hơn trong hoạt động sản xuất kinh doanh, tiếp tục góp <br><br> phần vào sự nghiệp phát triển “văn hóa đọc”, làm cho <br><br> những giá trị vĩnh hằng của sách ngày càng thấm sâu vào <br><br> đời sống văn hóa tinh thần của xã hội, nhằm góp phần tích <br><br> cực, đáp ứng yêu cầu nâng cao dân trí, bồi dưỡng nhân tài <br><br> và nguồn nhân lực cho sự nghiệp công nghiệp hóa, hiện <br><br> đại hóa đất nước .</html>"},
+        {"BẢO MẬT & HIỆU QUẢ", "secure_100px.svg",  "<html>Thông tin cá nhân và thông tin liên quan đến sách mượn <br><br> thường được bảo mật và chỉ được truy cập bởi người dùng <br><br> hoặc những người được ủy quyền Nhờ vào tính năng đặc <br><br> biệt của mã ISBN giúp xác định được thông tin về từng <br><br> cuốn sách một cách nhanh chóng và chính xác, giúp cho <br><br> việc quản lý sách được thực hiện một cách hiệu quả hơn..</html>"},
+        {"HƯỚNG PHÁT TRIỂN ", "effective_100px.svg","<html>Cửa hàng có thể cung cấp các sản phẩm sách tái chế hoặc <br><br> triển khai các hoạt động bảo vệ môi trường, như giảm <br><br> thiểu việc sử dụng túi nhựa, khuyến khích khách hàng tái <br><br> sử dụng túi vải, và tham gia vào các hoạt động cộng <br><br> đồng về bảo vệ môi trường. Mở rộng dịch vụ thu mua, trao <br><br> đổi và bán lại sách cũ, giúp khách hàng có thêm lựa chọn <br><br> và thúc đẩy sự bền vững trong việc sử dụng sách.</html>"},
     };
     Color MainColor = new Color(0x55BEC0);
     Color FontColor = new Color(96, 125, 139);
@@ -44,7 +44,7 @@ public class TrangChu extends JPanel {
         center = new JPanel();
         center.setBackground(BackgroundColor);
         center.setPreferredSize(new Dimension(1100, 800));
-        center.setLayout(new GridLayout(0 , 3 ,0,20));
+        center.setLayout(new GridLayout(1 , 3 , 10, 0));
         center.setBorder(new EmptyBorder(10,30,30,30));
 
 
