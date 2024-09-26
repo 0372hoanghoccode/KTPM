ccd89cc633fdef66478d848e794bfb795152e2f0
Hieuthu16 <truongnguyenminhhieu1000@gmail.com>
2024-09-27 04:24:04 +0700

    ẩn thêm

--- src/GUI/Panel/SanPhamKH.java
+++ src/GUI/Panel/SanPhamKH.java
@@ -81,7 +81,7 @@ public final class SanPhamKH extends JPanel implements ActionListener {
         functionBar.setLayout(new GridLayout(1, 2, 50, 0));
         functionBar.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        String[] action = {"create", "detail"};
+        String[] action = { "detail"};
         mainFunction = new MainFunction(m.user.getMNQ(), "xemhang", action);
         for (String ac : action) {
             mainFunction.btn.get(ac).addActionListener(this);
